ff775bb52de659170e5fd3b0a160e47e989d1dd5
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2022-01-07 18:07:04 +0000

    layout:improve

--- src/components/ScreenMessager/index.tsx
+++ src/components/ScreenMessager/index.tsx
@@ -6,8 +6,8 @@ interface Props {
 }
 
 export const ScreenMessager: React.FC<Props> = props => (
-  <div className="flex flex-col items-center mt-3">
-    {props.svg({ className: 'w-80 h-80' })}
+  <div className="flex flex-col items-center mt-3 w-full mx-2 max-w-sm">
+    {props.svg({ className: 'w-full h-full' })}
     <h1 className="max-w-xl text-center text-2xl">{props.text}</h1>
   </div>
 )
